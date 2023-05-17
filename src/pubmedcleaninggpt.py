bf3fdd6941616cdc34a6dfa5ead32ec54ac7b15f
Sam Savage <samuel.savage@uconn.edu>
2023-05-16 22:45:33 -0400

    updating data scraping mechinisms and more

--- src/pubmedcleaninggpt.py
+++ src/pubmedcleaninggpt.py
@@ -1,13 +1,20 @@
 from dotenv import load_dotenv
 from gpt4all import GPT4All
-
+import json
+from datetime import datetime
 load_dotenv()
-
+import pandas as pd 
 from google.cloud import bigquery
 from google.oauth2.service_account import Credentials
 import openai
 import pandas_gbq
 import scholarly
+
+
+with open('/Users/samsavage/PythonProjects/PubMedGPT/data/openaikeys.json') as f:
+    keys = json.load(f)
+
+openai_api_key = keys["OPENAI_API_KEY"]
 
 def get_total_citation_count(doi_str):
     # Split the DOIs using the delimiter
@@ -30,20 +37,20 @@ def get_total_citation_count(doi_str):
 max_tokens = 2153
 
 
-def get_free_conclusion(condition, truncated_text):
-    gptj = GPT4All("ggml-gpt4all-j-v1.3-groovy")
-    prompt = f"""Tell me the conclusion from this PubMed Paper on this {condition}: \n{truncated_text}\n\n"""
-    messages = [{"role": "user", "content": prompt}]
-    message = [0:2048!]
-    answer = gptj.chat_completion(messages)['choices'][0]['message']['content']
-    return str(answer),prompt
+# def get_free_conclusion(condition, truncated_text):
+#     gptj = GPT4All("ggml-gpt4all-j-v1.3-groovy")
+#     prompt = f"""Tell me the conclusion from this PubMed Paper on this {condition}: \n{truncated_text}\n\n"""
+#     messages = [{"role": "user", "content": prompt}]
+#     message = [0:2048]
+#     answer = gptj.chat_completion(messages)['choices'][0]['message']['content']
+#     return str(answer),prompt
 
 
 def insert_dataframe_into_table(df):
     # Instantiate a client object using credentials
     project_name = "airflow-test-371320"
     dataset_name = "BACKFILL"
-    table_id = f"{dataset_name}.BACKFILL_2023_GPT4_ALL"
+    table_id = f"{dataset_name}.BACKFILL_2023_GPT3"
     key_path = "/Users/samsavage/PythonProjects/PubMedGPT/data/gcp_creds.json"
     creds = Credentials.from_service_account_file(key_path)
     client = bigquery.Client(credentials=creds, project=project_name)
@@ -58,13 +65,18 @@ def insert_dataframe_into_table(df):
     )
 
 def get_clean_data():
+
     # Instantiate a client object using credentials
     project_name = "airflow-test-371320"
     key_path = "/Users/samsavage/PythonProjects/PubMedGPT/data/gcp_creds.json"
     creds = Credentials.from_service_account_file(key_path)
     client = bigquery.Client(credentials=creds, project=project_name)
-    query = f"""with puby as (
-    Select
+    query = f"""WITH ranked_table AS (
+    SELECT 
+        a.*,
+        ROW_NUMBER() OVER(PARTITION BY a.pmid_2 ORDER BY a.DatePublished DESC) as rankdaddy
+    FROM (
+Select
         concat(Year,"-",Month,"-",Day) as Date_,
         concat(Year,"-",(case Month
             WHEN 'jan' THEN '01'
@@ -82,28 +94,31 @@ def get_clean_data():
         Year,
         Month,
         Day,
-        *
-    from (
-        SELECT 
-            *,
-            REGEXP_EXTRACT(REGEXP_EXTRACT(xml_list, r'<PubDate>(.*?)<\/PubDate>'),r'\d{4}') as Year,
-            REGEXP_EXTRACT(REGEXP_EXTRACT(xml_list, r'<PubDate>(.*?)<\/PubDate>'),r'<Month>([a-zA-Z]+)</Month>') as Month,
-            REGEXP_EXTRACT(REGEXP_EXTRACT(xml_list, r'<PubDate>(.*?)<\/PubDate>'),r'<Day>(\d{1,2})</Day>') as Day,
-            REGEXP_EXTRACT(xml_list, r'<ArticleId IdType="doi">([^<]+)</ArticleId>') AS DOI
-        FROM `airflow-test-371320.BACKFILL.BACKFILL_2023_Clinical_Trials_vm`
-    )
+        * from (
+            SELECT 
+                *,
+                REGEXP_EXTRACT(REGEXP_EXTRACT(xml_list, r'<PubDate>(.*?)<\/PubDate>'), r'\d{4}') as Year,
+                REGEXP_EXTRACT(REGEXP_EXTRACT(xml_list, r'<PubDate>(.*?)<\/PubDate>'), r'<Month>([a-zA-Z]+)</Month>') as Month,
+                REGEXP_EXTRACT(REGEXP_EXTRACT(xml_list, r'<PubDate>(.*?)<\/PubDate>'), r'<Day>(\d{1,2})</Day>') as Day,
+                REGEXP_EXTRACT(xml_list, r'<ArticleId IdType="doi">([^<]+)</ArticleId>') AS DOI
+            FROM `airflow-test-371320.BACKFILL.BACKFILL_2023_Clinical_Trials_vm`
+        )
+    ) a 
 )
-
+Select z.*
+from (
 SELECT 
     SAFE_CAST(concat(PubDate,"-01") as date) as date_,
     ChronicCondition,
     concat(STRING_AGG(Conclusion,'++'),STRING_AGG(Abstract,'++')) AS raw_text,
     STRING_AGG(PMID_2,'++') AS PMID,
     STRING_AGG(DOI,'++') AS DOI
-FROM puby
+FROM ranked_table
 where PubDate is not null 
+and rankdaddy =1 
 GROUP BY 1,2
-ORDER BY date_ desc"""
+ORDER BY date_ desc) z
+where z.raw_text not in (Select raw_text FROM `airflow-test-371320.BACKFILL.BACKFILL_2023_GPT3`)"""
 
     query_job = client.query(query)
     results = query_job.result().to_dataframe()
@@ -115,7 +130,7 @@ def summarize_text(text, condition):
     """
     Summarize the given text using the DaVinci API.
     """
-    openai.api_key = "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"
+    openai.api_key = openai_api_key
 
     prompt_and_response_max_tokens = 4096
 
@@ -135,7 +150,7 @@ def summarize_text(text, condition):
         max_tokens=max_tokens,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.1,
     )
 
     summary = response.choices[0].text.strip()
@@ -169,8 +184,8 @@ def go():
         ):
             counter += 1
             try:
-                # sum, prompt = summarize_text(text, condition=condition)
-                sum,prompt = get_free_conclusion(text,condition)
+                sum, prompt = summarize_text(text, condition=condition)
+                # sum,prompt = get_free_conclusion(text,condition)
                 prompt_list.append(prompt)
                 text_list.append(sum)
                 print(f"done with row {counter}")
@@ -185,12 +200,17 @@ def go():
         chunk["GPT3_SUMMARY"] = text_list
         chunk["GPT3_PROMPT"] = prompt_list
         chunk["date_"] = chunk["date_"].astype("string")
+        chunk["date_uploaded"] = pd.to_datetime(datetime.now().strftime('%Y-%m-%d'))
+        chunk["date_uploaded_new"] = pd.to_datetime(datetime.now().strftime('%Y-%m-%d'))
+
         chunk =  chunk[["date_",				
-"ChronicCondition",			
-"raw_text",			
-"PMID",		
-"GPT3_SUMMARY",			
-"GPT3_PROMPT"]]
+                        "ChronicCondition",			
+                        "raw_text",			
+                        "PMID",		
+                        "GPT3_SUMMARY",			
+                        "GPT3_PROMPT",
+                        "date_uploaded",
+                        "date_uploaded_new"]]
         insert_dataframe_into_table(chunk)
         print("chunk inserted")
 
